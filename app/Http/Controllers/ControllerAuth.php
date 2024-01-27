85b5e5eda1f978c8222dba6366bb123ce67f1be9
JDavidHA <58662948+JDevArauz@users.noreply.github.com>
2024-01-27 15:41:17 -0600

    ControllerAuth UPDATE

--- app/Http/Controllers/ControllerAuth.php
+++ app/Http/Controllers/ControllerAuth.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -10,49 +11,48 @@ class ControllerAuth extends Controller
 {
     public function register(Request $request)
     {
-        $user = new User();
+        // Validación de datos
+        $request->validate([
+            'name' => 'required|string',
+            'idCard' => 'required|integer',
+            'firstLastName' => 'required|string',
+            'secondLastName' => 'required|string',
+            'phone' => 'required|integer',
+            'address' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
 
-        // Consulta para saber si el email ya existe en la BD
-        $verifiedEmail = User::query()->where('email', '=', $request->email)->exists();
+        $user = User::create([
+            'name' => $request->name,
+            'idCard' => $request->idCard,
+            'firstLastName' => $request->firstLastName,
+            'secondLastName' => $request->secondLastName,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'rol' => 1,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
-        if (!$this->userEmpty($request)) {
-            if (!$verifiedEmail) {
-                $user->name = $request->name;
-                $user->idCard = $request->idCard;
-                $user->firstLastName = $request->firstLastName;
-                $user->secondLastName = $request->secondLastName;
-                $user->phone = $request->phone;
-                $user->address = $request->address;
-                $user->rol = 1;
-                $user->email = $request->email;
-                $user->password = Hash::make($request->password);
+        Auth::login($user);
 
-                $user->save();
-
-                Auth::login($user);
-
-                return response()->json(['message' => 'Registro exitoso', 'user' => $user], 200);
-            } else {
-                return response()->json(['error' => 'El correo electrónico ya está registrado'], 400);
-            }
-        } else {
-            return response()->json(['error' => 'Todos los campos son obligatorios'], 400);
-        }
+        return response()->json(['message' => 'Registro exitoso', 'user' => $user], 200);
     }
 
     public function login(Request $request)
     {
-        $credentials = [
-            "email" => $request->email,
-            "password" => $request->password,
-        ];
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
 
-        $remember = ($request->has('remember') ? true : false);
+        $remember = $request->has('remember') ? true : false;
 
         if (Auth::attempt($credentials, $remember)) {
             $request->session()->regenerate();
             $user = User::where('email', $request->email)->firstOrFail();
-            
+
             $token = $user->createToken('access_token')->plainTextToken;
 
             return response()->json(['message' => 'Inicio de sesión exitoso', 'user' => $user, 'access_token' => $token], 200);
@@ -71,19 +71,90 @@ class ControllerAuth extends Controller
         return response()->json(['message' => 'Cierre de sesión exitoso'], 200);
     }
 
-    // Se valida que los datos del registro no vayan vacíos
-    private function userEmpty(Request $request)
+    public function forgotPassword(Request $request)
     {
-        if (
-            $request->name == null || $request->idCard == null || $request->firstLastName == null ||
-            $request->secondLastName == null || $request->phone == null || $request->address == null ||
-            $request->email == null || $request->password == null
-        ) {
-            return true;
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json(['error' => 'Correo electrónico no encontrado'], 404);
         }
 
-        return false;
-    }
-}
+        $resetToken = $this->generateResetToken($user);
 
-?>
+        // Envía el correo de recuperación de contraseña
+        Mail::to($user->email)->send(new ResetPasswordMail($resetToken));
+
+        return response()->json(['message' => 'Correo de recuperación enviado exitosamente'], 200);
+    }
+
+    private function generateResetToken(User $user)
+    {
+        $token = sha1(time() . $user->email . $user->password);
+
+        // Guarda el token en la base de datos o en una tabla específica para restablecimiento de contraseña
+        // Esto puede depender de tu implementación específica
+
+        // Ejemplo:
+        // $user->update(['reset_token' => $token]);
+
+        return $token;
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'token' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json(['error' => 'Correo electrónico no encontrado'], 404);
+        }
+
+        // Validar el token (puedes tener una lógica específica aquí)
+        $isValidToken = $this->validateResetToken($user, $request->token);
+
+        if (!$isValidToken) {
+            return response()->json(['error' => 'Token no válido'], 400);
+        }
+
+        // Actualizar la contraseña
+        $user->update([
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Eliminar el token de restablecimiento de contraseña
+        // Esto puede depender de tu implementación específica
+
+        // Ejemplo:
+        // $user->update(['reset_token' => null]);
+
+        return response()->json(['message' => 'Contraseña restablecida exitosamente'], 200);
+    }
+
+    private function validateResetToken(User $user, $token)
+    {
+        // Obtener todos los tokens de acceso del usuario
+        $userTokens = $user->tokens;
+    
+        // Validar si el token de restablecimiento es válido
+        $isValidToken = $userTokens->contains(function ($userToken) use ($token) {
+            // Puedes comparar el token almacenado con el token proporcionado
+            // Aquí necesitarás ajustar según cómo almacenes y compares tus tokens
+    
+            // Ejemplo: Comparar solo el valor del token
+            return hash_equals($userToken->id, $token);
+        });
+    
+        return $isValidToken;
+    }
+    
+
+}
