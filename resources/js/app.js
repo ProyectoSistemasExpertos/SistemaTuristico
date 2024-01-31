0f4d4c912fdcb6dce2cafaaea99ec0445c74b28b
JDavidHA <58662948+JDevArauz@users.noreply.github.com>
2024-01-30 23:32:51 -0600

    REPAIR POOP OF MAURICIOS

--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import './bootstrap';
-import 'slick-carousel/slick/slick.min';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
--- resources/views/privada.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-
-    <h1>Seccion Privada</h1>
-
-    <a href="{{route('logout')}}"><button type="button" class="btn btn-outline-primary me-2">Salir</button></a>
-</div>
