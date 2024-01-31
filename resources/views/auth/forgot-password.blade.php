763370ad3d2cbd8ef46e83ffbed9a8ec9e8d8217
JDavidHA <58662948+JDevArauz@users.noreply.github.com>
2024-01-30 22:13:06 -0600

    BOTON VOLVER EN FORGOT PASS

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+<a  href="{{ route('show-login') }}" class="flex mb-2 justify-right w-14  bg-purple-800 hover:bg-purple-700 text-gray-100 p-1 rounded-lg tracking-wide font-semibold cursor-pointer transition ease-in duration-500">Volver</a>
 <div class="row justify-content-center">
     <div class="col-md-35">
         <div class="card-body">
             <form method="POST" action="{{ route('forgot-link') }}">
-                @csrf
+
+             @csrf
                 <div class="mb-7">
                     <h3 class="font-semibold text-2xl text-gray-800">¿Olvidaste tu Contraseña?</h3>
                     <p class="text-gray-400">
@@ -28,5 +30,4 @@
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
-
 @endsection
